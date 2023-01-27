a7a7c2e645537f8d3dc516dfb3622c159c0c9813
Leo Camacho <leocamachocr@gmail.com>
2023-01-27 07:25:56 -0600

    Updates

---
+++ precache-manifest.dab523c998430bfc488c55f413b74b64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fd26a563a27b861a108",
+    "url": "/docs-revealjs/static/css/main.cc498f96.chunk.css"
+  },
+  {
+    "revision": "5fd26a563a27b861a108",
+    "url": "/docs-revealjs/static/js/main.5fd26a56.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/docs-revealjs/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/docs-revealjs/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "10e13cf32951befe5cbe",
+    "url": "/docs-revealjs/static/js/runtime~main.10e13cf3.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/docs-revealjs/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "fa4df8a23b351ee743873cdc4bf1bc05",
+    "url": "/docs-revealjs/index.html"
+  }
+];
